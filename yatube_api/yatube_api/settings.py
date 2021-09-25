c13d9fb4fba0e92cbe3ed3a135220e2b7719f620
Andrey Kapkaev <kapkaew@yandex.ru>
2021-09-25 11:50:57 +0300

    add app "djoser" and default auth class

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     'api',
     'posts',
 ]
@@ -89,5 +90,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
