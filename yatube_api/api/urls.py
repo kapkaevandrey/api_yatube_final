4026fe1251a28a813859e2f29d765b418746f3c6
Andrey Kapkaev <kapkaew@yandex.ru>
2021-09-25 11:51:12 +0300

    add urls

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import PostViewSet, GroupViewSet, CommentViewSets
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'posts/(?P<post_id>\d+)/comments',
+                CommentViewSets, basename="comments")
 
 urlpatterns = [
+    path(r'v1/', include('djoser.urls.jwt')),
+    path('v1/', include(router.urls))
 ]
