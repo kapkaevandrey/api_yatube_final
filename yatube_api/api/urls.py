c06e1371d2d6dcdef09c50acad206a6fa91c951b
kapkaevandrey <64920599+kapkaevandrey@users.noreply.github.com>
2021-09-27 23:12:40 +0300

    fix urls name and router name

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -3,14 +3,14 @@ from rest_framework import routers
 
 from .views import PostViewSet, GroupViewSet, CommentViewSets, FollowViewSets
 
-router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'groups', GroupViewSet)
-router.register(r'follow', FollowViewSets)
-router.register(r'posts/(?P<post_id>\d+)/comments',
-                CommentViewSets, basename="comments")
+router_v1 = routers.DefaultRouter()
+router_v1.register(r'v1/posts', PostViewSet, basename="posts")
+router_v1.register(r'v1/groups', GroupViewSet, basename="groups")
+router_v1.register(r'v1/follow', FollowViewSets, basename="follows")
+router_v1.register(r'v1/posts/(?P<post_id>\d+)/comments',
+                   CommentViewSets, basename="comments")
 
 urlpatterns = [
     path(r'v1/', include('djoser.urls.jwt')),
-    path('v1/', include(router.urls))
+    path('', include(router_v1.urls))
 ]
