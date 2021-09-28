3d8d70ddfb2af92240470df0e1d724e69bd3561c
kapkaevandrey <64920599+kapkaevandrey@users.noreply.github.com>
2021-09-28 08:50:42 +0300

    fix some style errors and permissions in views

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,26 +1,22 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import mixins
 from rest_framework import permissions
+from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from posts.models import Post, Group, Comment, Follow
-from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+from .permissions import AuthorOrReadOnly
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 from .serializers import FollowSerializer
-from .permissions import AuthorOrReadOnly, ReadOnly
+from posts.models import Comment, Follow, Group, Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (AuthorOrReadOnly,)
+    permission_classes = (AuthorOrReadOnly, IsAuthenticatedOrReadOnly)
     pagination_class = LimitOffsetPagination
-
-    def get_permissions(self):
-        if self.action == 'retrieve':
-            return (ReadOnly(),)
-        return super().get_permissions()
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -34,32 +30,26 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSets(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (AuthorOrReadOnly,)
+    permission_classes = (AuthorOrReadOnly, IsAuthenticatedOrReadOnly)
 
     def get_queryset(self):
-        post = get_object_or_404(Post, pk=self.kwargs["post_id"])
-        return Comment.objects.filter(post=post)
-
-    def get_permissions(self):
-        if self.action == 'retrieve':
-            return (ReadOnly(),)
-        return super().get_permissions()
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        return post.comments
 
     def perform_create(self, serializer):
-        post = get_object_or_404(Post, pk=self.kwargs["post_id"])
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
         serializer.save(author=self.request.user,
                         post=post)
 
 
-class FollowViewSets(viewsets.ReadOnlyModelViewSet,
-                     mixins.CreateModelMixin):
-    queryset = Follow.objects.all()
+class FollowViewSets(mixins.CreateModelMixin,
+                     viewsets.ReadOnlyModelViewSet):
     serializer_class = FollowSerializer
     filter_backends = (SearchFilter,)
-    search_fields = ("following__username",)
+    search_fields = ("^following__username",)
 
     def get_queryset(self):
-        return Follow.objects.filter(user=self.request.user)
+        return self.request.user.followers
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
