1c6aec0ca8f634d5cfd100e7a35bf16fa598341c
kapkaevandrey <64920599+kapkaevandrey@users.noreply.github.com>
2021-09-28 08:49:52 +0300

    Refactor Permission, delete unused class ReadOnly

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,14 +2,6 @@ from rest_framework import permissions
 
 
 class AuthorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
-
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
-
-
-class ReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
+        return (obj.author == request.user
+                or request.method in permissions.SAFE_METHODS)
