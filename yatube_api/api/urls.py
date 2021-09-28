fffe5b1834edac2f0aa58298331dbc6785ee76c9
kapkaevandrey <64920599+kapkaevandrey@users.noreply.github.com>
2021-09-28 13:08:44 +0300

    fix style problems

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,13 +4,13 @@ from rest_framework import routers
 from .views import PostViewSet, GroupViewSet, CommentViewSets, FollowViewSets
 
 router_v1 = routers.DefaultRouter()
-router_v1.register(r'v1/posts', PostViewSet, basename="posts")
-router_v1.register(r'v1/groups', GroupViewSet, basename="groups")
-router_v1.register(r'v1/follow', FollowViewSets, basename="follows")
-router_v1.register(r'v1/posts/(?P<post_id>\d+)/comments',
+router_v1.register(r'posts', PostViewSet, basename="posts")
+router_v1.register(r'groups', GroupViewSet, basename="groups")
+router_v1.register(r'follow', FollowViewSets, basename="follows")
+router_v1.register(r'posts/(?P<post_id>\d+)/comments',
                    CommentViewSets, basename="comments")
 
 urlpatterns = [
     path(r'v1/', include('djoser.urls.jwt')),
-    path('', include(router_v1.urls))
+    path('v1/', include(router_v1.urls))
 ]
